bc021f3fde4abf736f6ab690179be855656e600f
lojoyu <sophie200429@gmail.com>
2019-11-26 14:46:36 +0800

    Updates

---
+++ precache-manifest.023d06a2edc9cba8ee1417639cbc3f7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f639c2a183a841a1b408574006f3c7a9",
+    "url": "/speaking/index.html"
+  },
+  {
+    "revision": "e7be6cc7dbe2cca66165",
+    "url": "/speaking/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "17b8681806e924b2f00d",
+    "url": "/speaking/static/js/2.0a3c1c2f.chunk.js"
+  },
+  {
+    "revision": "e7be6cc7dbe2cca66165",
+    "url": "/speaking/static/js/main.ac95c56a.chunk.js"
+  },
+  {
+    "revision": "388c33f13b200a681ed8",
+    "url": "/speaking/static/js/runtime-main.477aa4a2.js"
+  }
+]);
