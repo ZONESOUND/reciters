2a201a3725c44eefaeeac94de0b8f7148255231f
lojoyu <sophie200429@gmail.com>
2019-12-05 19:07:40 +0800

    Updates

---
+++ precache-manifest.ecb3678f1f196480519ccc8a73d4dd8c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73fc7d5f1b156aa37f9ded8538d2f802",
+    "url": "/speaking/index.html"
+  },
+  {
+    "revision": "43bb0f11abc28f947f64",
+    "url": "/speaking/static/css/main.80af28f5.chunk.css"
+  },
+  {
+    "revision": "8d26ace8ac20171abd74",
+    "url": "/speaking/static/js/2.7448a44d.chunk.js"
+  },
+  {
+    "revision": "43bb0f11abc28f947f64",
+    "url": "/speaking/static/js/main.b752483d.chunk.js"
+  },
+  {
+    "revision": "388c33f13b200a681ed8",
+    "url": "/speaking/static/js/runtime-main.477aa4a2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/speaking/precache-manifest.d78d23dd6acac6a358a5e71d20ff2c6e.js"
+  "/speaking/precache-manifest.ecb3678f1f196480519ccc8a73d4dd8c.js"
 );
 
 self.addEventListener('message', (event) => {
